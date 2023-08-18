e3f978cb052f066e89915380f3feb7b24627f9c6
madscodess <32851804+madscodess@users.noreply.github.com>
2023-08-18 20:49:33 +0100

    getting data from API

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const searchButton = document.querySelector('#searchButton');
 const form = document.querySelector('#searchInput');
 
+const current = document.querySelector('#currentTemperature');
 
 searchButton.addEventListener("click", function(e){
   e.preventDefault()
@@ -11,15 +12,31 @@ searchButton.addEventListener("click", function(e){
 
   //set API key & url with input.city 
   let apiKey = "bb9c326b337a705c684adf14dac0abf6";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${apiKey}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&units=metric&appid=${apiKey}`;
 
 //get data using axios request 
 const getCityData = async () => {
     try{
         const response = await axios.get(`${apiUrl}`);
         const cityData = response.data;
-        console.log(`GET: Here's the list of weather`, cityData);
-        return cityData
+        console.log(`GET: Here's the list of weather in ${inputCity}`, cityData);
+     
+        //current temperature
+        //change numbers to two digits 
+        const currentTemp = String(cityData.main.temp).substring(0, 2);
+        current.innerHTML = ` ${currentTemp}°C`;
+        // current.style.
+        console.log(Number(currentTemp));
+
+        //max temperature
+        //change numbers to one digit 
+        const maxTemp = String(cityData.main.temp_max).substring(0, 2);
+        console.log(Number(maxTemp));
+       
+        //Min temperature
+        //change numbers to one digit 
+        const minTemp = String(cityData.main.temp_min).substring(0, 2);
+        console.log(Number(minTemp));
     }
     catch (e) {
             console.e(errors);
@@ -30,31 +47,3 @@ return getCityData();
 
 });
 
-
-
-  
-//   const inputVal = input.value;
-  
-//   const getCityData = async () => {
-//     try{
-//         const response = await axios.get(`${apiUrl}`);
-//         const cityData = response.data;
-//         console.log(`GET: Here's the list of weather`, cityData);
-//     }
-
-
-
-
-
-
-// //search 
-// //THIS CHANGE TO SUBMIT??
-// searchButton.addEventListener('click', function() {
-//     // get the search input value
-//     const searchTerm = searchInput.value;
-//     const api = `Searching for "https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=bb9c326b337a705c684adf14dac0abf6 ${searchTerm}"...`
-//     // do something with the search term (e.g. redirect to a search results page)
-//   //https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=bb9c326b337a705c684adf14dac0abf6
-//     console.log(api);
-//   });
-      
