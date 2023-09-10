3c2694b6d0b7f4630b05cf7ad50c63467dd275f4
madscodess <32851804+madscodess@users.noreply.github.com>
2023-09-10 17:39:16 +0100

    css update

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 const searchButton = document.querySelector('#searchButton');
 const form = document.querySelector('#searchInput');
 
+let weatherContainer = document.querySelector('div.container-fluid');
 let current = document.querySelector('#currentTemperature');
 let humidity = document.querySelector('p#humidity');
 let maxTempDiv = document.querySelector('p#max-temperature');
 let minTempDiv = document.querySelector('p#min-temperature');
 let sunrise = document.querySelector('#sunrise');
 let sunset = document.querySelector('#sunset');
+let images = document.querySelector('.card-img-top');
+
+
 //const highlightsDiv = document.getElementById('highlights');
 
 //current weather in Manchester as homepage 
@@ -75,6 +79,8 @@ searchButton.addEventListener("click", function(e){
   let apiKey = "bb9c326b337a705c684adf14dac0abf6";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&units=metric&appid=${apiKey}`;
   
+  //display containers
+  weatherContainer.style.display = "flex";
 
 //get data using axios request 
 const getCityWeather = async (errors) => {
@@ -82,12 +88,16 @@ const getCityWeather = async (errors) => {
         const response = await axios.get(`${apiUrl}`);
         const cityData = response.data;
         console.log(`GET: Here's the list of weather in ${inputCity}`, cityData);
- 
+        
+
         //current temperature
         //change numbers to two digits 
         let currentTemp = String(cityData.main.temp).substring(0, 2);
         console.log(currentTemp);
-        current.innerText = `${currentTemp}°C`   
+        current.innerText = `${currentTemp}°C`;
+
+        images.appendChild = cityData.weather[0].icon;
+        console.log(`imagesicon${images}`);
         
 
         //humidity percentage 
@@ -146,5 +156,4 @@ return getCityWeather();
 //   }
 //       return getManchesterForecast();
 // });
-
 
